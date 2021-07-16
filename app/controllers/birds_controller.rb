144267d2c1941e4df2afdc7c25f449e172621bc7
Jacob Klimenko <droidedjake@gmail.com>
2021-07-15 21:53:30 -0400

    commit tree 9bcb558eb08e6553ba62b4830bea59971d6402ec

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,24 @@
 class BirdsController < ApplicationController
+  
   def index
     @birds = Bird.all
-    render 'birds/index.html.erb'
+    birds = Bird.all
+
+  ## ERB
+  #
+  #   render 'birds/index.html.erb'
+
+  ## Plain Text
+  #   render plain: "Hello #{@birds[3].name}"
+
+  ## JSON
+  #   render json: 'String data'
+  #   render json: { message: 'Hashes convert to JSON'}
+  #   render json: ['Arrays', 'too']
+  #   render json: @birds
+  #   render json: birds
+  #   render json: {birds: @birds, messages: ['Hey Birds', 'Bye Birds']}
+  #   render json: {birds: @birds, messages: ['Hey Birds', 'Bye Birds']}.to_json
   end
+
 end
